5dfb69be53874a58895e15ab731722eeecf3461c
Paul Koos <paully_k5298@yahoo.com>
2022-03-17 19:11:34 -0700

    verify that TileSet returns an array of 9 tiles when generated.

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -5,7 +5,8 @@ describe 'A tile set' do
   describe '::generate' do
 
     it 'returns an array of 9 tiles by default' do
-      raise "Start here"
+      expect(TileSet.generate.length).to eq(9)
+      expect(TileSet.generate.is_a?(Array)).to be_truthy
     end
 
     it 'returns an array of tiles of a specified length' do
