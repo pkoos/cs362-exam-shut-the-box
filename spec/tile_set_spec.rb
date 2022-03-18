8a88d9a45cf4b911477ec62dfa85ba1398ffaff6
Paul Koos <paully_k5298@yahoo.com>
2022-03-17 19:25:05 -0700

    test that various tiles are assigned the correct value when the tileset is generated.

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -7,14 +7,36 @@ describe 'A tile set' do
     it 'returns an array of 9 tiles by default' do
       expect(TileSet.generate.length).to eq(9)
       expect(TileSet.generate.is_a?(Array)).to be_truthy
+      expect(TileSet.generate[7].is_a?(Tile)).to be_truthy
     end
 
     it 'returns an array of tiles of a specified length' do
-      skip
+      length = 7
+      expect(TileSet.generate(length).length).to eq(7)
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
-      skip
+      counter = 0
+      value=counter + 1
+      max_value = 9
+      tileset = TileSet.generate
+
+      tile = tileset[counter]
+      expect(tile.value).to eq(value)
+      
+      counter += 1
+      value += 1
+      tile = tileset[counter]
+      expect(tile.value).to eq(value)
+
+      counter += 6
+      value += 6
+      tile = tileset[counter]
+      expect(tile.value).to eq(value)
+
+      last_tile = tileset[-1]
+      expect(last_tile.value).to eq(max_value)
+      
     end
 
   end
